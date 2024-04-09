fca391ff6e064dc12e5c34f73d419742a56360cd
Tóth Róbert <tothr94@gmail.com>
2024-04-09 21:48:01 +0200

    Fix typo

--- week-08/samples/test_args.c
+++ week-08/samples/test_args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[], char *ev)
+int main(int argc, char *argv[])
 {
     printf("argc: %d\n", argc);
     for (int i = 0; i < argc; i++)
